95f5db708a4bb43bebc277f5f9e6f922f033308a
Moutinho <alexandre.moutinho@live.fr>
2021-03-05 14:42:51 +0100

    :lipstick: correction of typing error on the description

--- js/panier.js
+++ js/panier.js
@@ -12,7 +12,7 @@ if (baskets.length < 1) {
   <div class="container my-3 py-1">
     <div class="text-center my-5 shadow p-3 mb-5 bg-body rounded">
       <h1 class="fs-1 font-weight-bold text-secondary my-5">Votre panier semble vide, commencez vos achats</h1>
-      <a role="button" class="btn btn-success text-uppercase my-3" href="index.html">ça se passe par içi</a> 
+      <a role="button" class="btn btn-success text-uppercase my-3" href="index.html">ça se passe par ici</a> 
     </div>
   </div>`;
 } else {
